d8ed0e142e81e5833968b7bb6231f0438613995d
RentarouAkari <40298630+RentarouAkari@users.noreply.github.com>
2018-06-16 11:52:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -77,6 +77,16 @@ client.on("message", async message => {
     return message.channel.send(serverembed);
   }
 
+});
+
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
 
   if(cmd === `${prefix}botinfo`){
 
@@ -91,7 +101,6 @@ client.on("message", async message => {
 
     return message.channel.send(botembed);
   }
-
 
 });
 
