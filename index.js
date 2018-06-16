5dfe029acd08b83a435d3bd877a72034ef8c4863
RentarouAkari <40298630+RentarouAkari@users.noreply.github.com>
2018-06-16 11:59:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -77,6 +77,21 @@ client.on("message", async message => {
     return message.channel.send(serverembed);
   }
 
+  if(cmd === `${prefix}botinfo`){
+
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+    .setDescription("Bot Information")
+    .setColor("#15f153")
+    .setThumbnail(bicon)
+    .addField("Bot Name", bot.user.username)
+    .addField("Created On", bot.user.createdAt);
+
+    return message.channel.send(botembed);
+
+  }
+
+
 });
 
 client.login(process.env.TOKEN);
